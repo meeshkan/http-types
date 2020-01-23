6a7c914d0cceb77558183c2bb82976ba974793b0
Fredrik Fornwall <fredrik@fornwall.net>
2020-01-24 00:29:02 +0100

    Verify sample in README against schema

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e -u
 
+sed -n '/^```json/,/^```/p;/^pattern2/q' README.md | sed '1d' | sed '$ d' \
+    > from-readme.sample.json
+
 for file in *sample.json; do
     echo "Verifying $file against schema..."
     jsonschema -i $file http-types-schema.json
