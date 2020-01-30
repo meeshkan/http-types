4aa3c3c8ccc2a211aeeb2b0cb2214e7d887e9190
Fredrik Fornwall <fredrik@fornwall.net>
2020-01-30 06:54:27 +0100

    Allow array values for query and header

--- test.sh
+++ test.sh
@@ -2,9 +2,14 @@
 set -e -u
 
 sed -n '/^```json/,/^```/p;/^pattern2/q' README.md | sed '1d' | sed '$ d' \
-    > from-readme.sample.json
+    > valid/from-readme.json
 
-for file in *sample.json; do
+for file in valid/*.json; do
     echo "Verifying $file against schema..."
     jsonschema -i $file http-types-schema.json
 done
+
+#for file in invalid/*.json; do
+    #echo "Verifying $file against schema..."
+    #jsonschema -i $file http-types-schema.json
+#done
